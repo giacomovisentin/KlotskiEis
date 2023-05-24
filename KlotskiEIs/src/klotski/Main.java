dae0372fd38f937ddbf8e387e428b2bb69ca02cd
giacomo <giacomovise01@gmail.com>
2023-05-24 14:06:28 +0200

    tolte righe prova

--- KlotskiEIs/src/klotski/Main.java
+++ KlotskiEIs/src/klotski/Main.java
@@ -5,12 +5,10 @@ import klotski.view.KlotskiApp;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Board b = new Board();
 
 		KlotskiApp app = new KlotskiApp(b);
 		
 		app.setVisible(true);
-		
 	}
 }
